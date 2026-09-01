73ed9d1875946eb460a1e033bd311093824c947e
zelinsky alexander <34163821+zelinsky-alexander@users.noreply.github.com>
2026-09-01 13:35:55 +0300

    Enable MS4.1 storage scheduling

--- src/main/java/dev/neta/coordinator/CoordinatorApplication.java
+++ src/main/java/dev/neta/coordinator/CoordinatorApplication.java
@@ -1,12 +1,15 @@
 package dev.neta.coordinator;
 
 import dev.neta.coordinator.config.CoordinatorProperties;
+import dev.neta.coordinator.config.CoordinatorStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties(CoordinatorProperties.class)
+@EnableScheduling
+@EnableConfigurationProperties({CoordinatorProperties.class, CoordinatorStorageProperties.class})
 public class CoordinatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoordinatorApplication.class, args);
